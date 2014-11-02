453d25b76faf4c1dc62e1cec09ca05eca11c745c
Julio Canares <juliocanares@gmail.com>
2014-11-02 01:02:18 -0700

    load contribs

--- gruntfile.js
+++ gruntfile.js
@@ -1,1 +1,11 @@
-gruntfile.js
+/**
+ * Author : www.juliocanares.com/cv
+ * Email : juliocanares@gmail.com
+ */
+
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+};
