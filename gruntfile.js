abdee3c4c7d5d45459bcb8176b4e278101d00d06
Julio Canares <juliocanares@gmail.com>
2014-11-02 01:02:58 -0700

    add concat task

--- gruntfile.js
+++ gruntfile.js
@@ -4,6 +4,19 @@
  */
 
 module.exports = function (grunt) {
+    var srcFiles = ['./app/src/**/*.js'];
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                stripBanners: true
+            },
+            dist: {
+                src: srcFiles,
+                dest: './public/js/app.dev.js'
+            }
+        }
+    });
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
