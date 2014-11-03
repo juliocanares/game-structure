36cb4a7c1573e1c2b30b13f5c005d903f0ddba4b
Julio Canares <juliocanares@gmail.com>
2014-11-03 01:55:18 -0800

    add handlers to manager clean and add screen

--- app/js/managers/ScreenManager.js
+++ app/js/managers/ScreenManager.js
@@ -10,7 +10,17 @@ APP.ScreenManager = function () {
     this.addChild(this.screenContainer);
     
     this.currentScreen = null;
+
+    this.listeners();
 };
 APP.ScreenManager.prototype = Object.create(PIXI.DisplayObjectContainer.prototype);
 
 APP.ScreenManager.constructor = APP.ScreenManager;
+
+APP.ScreenManager.prototype.listeners = function () {
+    this.goScreenCleanHandlerBind = this.goScreenCleanHandler.bind(this);
+    this.goScreenChangeHandlerBind = this.goScreenChangeHandler.bind(this);
+
+    Broadcaster.listen('GO_SCREEN_CLEAN', this.goScreenCleanHandlerBind);
+    Broadcaster.listen('GO_SCREEN_CHANGE', this.goScreenChangeHandlerBind);
+};
