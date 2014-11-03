4d7af7f81452fa942c0c7afb4c29d0ba7128c162
Julio Canares <juliocanares@gmail.com>
2014-11-03 01:54:54 -0800

    add screenContainer and currentScreen variables

--- app/js/managers/ScreenManager.js
+++ app/js/managers/ScreenManager.js
@@ -6,6 +6,10 @@
 APP.ScreenManager = function () {
     PIXI.DisplayObjectContainer.call(this);
 
+    this.screenContainer = new PIXI.DisplayObjectContainer();
+    this.addChild(this.screenContainer);
+    
+    this.currentScreen = null;
 };
 APP.ScreenManager.prototype = Object.create(PIXI.DisplayObjectContainer.prototype);
 
