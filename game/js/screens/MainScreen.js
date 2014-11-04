3a79f4250f3e14564fa1d967da8b80e820b0e174
Julio Canares <juliocanares@gmail.com>
2014-11-04 02:23:33 -0800

    add mainScreen

---
+++ game/js/screens/MainScreen.js
@@ -0,0 +1,13 @@
+/**
+ *Author : www.juliocanares.com/cv
+ *Email : juliocanares@gmail.com
+ */
+
+var Game = Game || {};
+
+Game.MainScreen = function () {
+    Game.BaseScreen.call(this);
+};
+
+Game.MainScreen.constructor = Game.MainScreen;
+Game.MainScreen.prototype = Object.create(Game.BaseScreen.prototype);
